0da9999ca44ea9b066c615eecc9b4da48fd735d3
Lucas Löffel <lucas@loeffel.io>
2020-01-29 00:33:47 +0100

    typo

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -33,7 +33,7 @@ module.exports = {
         link: 'https://twys.io/imprint',
       },
       {
-        text: 'Imprint',
+        text: 'Privacy',
         link: 'https://twys.io/privacy',
       },
     ],
