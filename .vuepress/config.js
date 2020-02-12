5680c7f1528792eb2f9be849ee5a471d2c05e2ef
Lucas Löffel <lucas@loeffel.io>
2020-02-12 18:26:01 +0100

    add google tag manager

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -34,7 +34,18 @@ module.exports = {
           r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
           a.appendChild(r);
         })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-    `,
+      `,
+    ],
+    [
+      'script',
+      {},
+      `
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-W7WKJZ7');
+      `,
     ],
     [
       'link',
