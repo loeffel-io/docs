384e96d878ecb857c505f747ba45c36d6bca7fcc
Lucas Löffel <lucas@loeffel.io>
2020-02-24 15:42:05 +0100

    update makeless

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-curl -sL -o makeless https://github.com/makeless/makeless/releases/download/v1.0.0/makeless-linux && \
+curl -sL -o makeless https://github.com/makeless/makeless/releases/download/v1.1.0/makeless-linux && \
     chmod +x makeless && \
     TOKEN="$MAKELESS_TOKEN" ./makeless
